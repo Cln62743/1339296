a750d243ad5e6b3996c61aeacedf90f0cc13aa6d
Charles <charles.langevin@hotmail.com>
2018-10-29 12:24:38 -0400

    Atelier: 10 Étape 4 a 6: Terminée
    Activite:
    - modification de fournirAction -> ajout de l'action connexion
    - Création et remplissage de fournirActionConnexion
    - Création et remplissage de connexion
    - Création et remplissage de onActivityResult
    - Création et remplissage de deconnexion (Pas encore utilise)
    
    Vue:
    - modification de recupererControles -> recupere le bouton connexion
    - modification de demanderActions -> crée l'action connexion
    - modification de installerListenerPartie -> installe le listener connexion
    - Création et remplissage de installerListenerConnexion
    
    GConstantes:
    - Ajout de la constante MA_CONSTANTE_CODE_CONNEXION -> Valeur: 123
    
    GCommande:
    - Ajout de la commande CONNEXION

--- app/src/main/java/ca/cours5b5/charleslangevin/activites/AMenuPrincipal.java
+++ app/src/main/java/ca/cours5b5/charleslangevin/activites/AMenuPrincipal.java
@@ -2,13 +2,22 @@ package ca.cours5b5.charleslangevin.activites;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ca.cours5b5.charleslangevin.R;
 import ca.cours5b5.charleslangevin.controleurs.ControleurAction;
 import ca.cours5b5.charleslangevin.controleurs.interfaces.Fournisseur;
 import ca.cours5b5.charleslangevin.controleurs.interfaces.ListenerFournisseur;
 import ca.cours5b5.charleslangevin.global.GCommande;
+import ca.cours5b5.charleslangevin.global.GConstantes;
 
 public class AMenuPrincipal extends Activite implements Fournisseur {
 
@@ -22,50 +31,91 @@ public class AMenuPrincipal extends Activite implements Fournisseur {
     }
 
     private void fournirActions() {
-
         fournirActionOuvrirMenuParametres();
         fournirActionDemarrerPartie();
+        fournirActionConnexion();
     }
 
     private void fournirActionOuvrirMenuParametres() {
-
         ControleurAction.fournirAction(this,
                 GCommande.OUVRIR_MENU_PARAMETRES,
                 new ListenerFournisseur() {
                     @Override
                     public void executer(Object... args) {
-
                         transitionParametres();
-
                     }
                 });
     }
 
     private void fournirActionDemarrerPartie() {
-
         ControleurAction.fournirAction(this,
                 GCommande.DEMARRER_PARTIE,
                 new ListenerFournisseur() {
                     @Override
                     public void executer(Object... args) {
-
                         transitionPartie();
+                    }
+                });
+    }
 
+    private void fournirActionConnexion() {
+        ControleurAction.fournirAction(this,
+                GCommande.CONNEXION,
+                new ListenerFournisseur() {
+                    @Override
+                    public void executer(Object... args) {
+                        connexion();
                     }
                 });
     }
 
     private void transitionParametres(){
-
         Intent intentionParametres = new Intent(this, AParametres.class);
         startActivity(intentionParametres);
-
     }
 
     private void transitionPartie(){
-
         Intent intentionParametres = new Intent(this, APartie.class);
         startActivity(intentionParametres);
+    }
 
+    private void connexion(){
+        List<AuthUI.IdpConfig> fournisseursDeConnexion = new ArrayList<>();
+
+        fournisseursDeConnexion.add(new AuthUI.IdpConfig.GoogleBuilder().build());
+        fournisseursDeConnexion.add(new AuthUI.IdpConfig.EmailBuilder().build());
+        fournisseursDeConnexion.add(new AuthUI.IdpConfig.PhoneBuilder().build());
+
+        Intent intentionConnexion = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(fournisseursDeConnexion)
+                .build();
+
+        this.startActivityForResult(intentionConnexion, GConstantes.MA_CONSTANTE_CODE_CONNEXION);
+    }
+
+    @Override
+    protected void onActivityResult(int reqestCode, int resultCode, Intent data){
+        if(reqestCode == GConstantes.MA_CONSTANTE_CODE_CONNEXION){
+            if(resultCode == RESULT_OK){
+                Log.d("Atelier11","AMenuPrincipal::Connexion reussie");
+                // Connexion reussie
+            }else{
+                Log.d("Atelier11","AMenuPrincipal::Connexion echouee");
+                // Connexion echouee
+            }
+        }
+    }
+
+    private void deconnexion(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("Atelier11","AMenuPrincipal::Deconnexion echouee");
+                        // Deconnexion reussie
+                    }
+                });
     }
 }
--- app/src/main/java/ca/cours5b5/charleslangevin/vues/VMenuPrincipal.java
+++ app/src/main/java/ca/cours5b5/charleslangevin/vues/VMenuPrincipal.java
@@ -14,11 +14,14 @@ import ca.cours5b5.charleslangevin.global.GCommande;
 
 public class VMenuPrincipal extends Vue {
 
+    private Button boutonPartie;
+    private Action actionPartie;
+
     private Button boutonParametres;
     private Action actionParametres;
 
-    private Button boutonPartie;
-    private Action actionPartie;
+    private Button boutonConnexion;
+    private Action actionConnexion;
 
     public VMenuPrincipal(Context context) {
         super(context);
@@ -38,59 +41,54 @@ public class VMenuPrincipal extends Vue {
         super.onFinishInflate();
 
         recupererControles();
-
         demanderActions();
-
         installerListeners();
-
     }
 
 
     private void recupererControles() {
-
-        boutonParametres = findViewById(R.id.bouton_parametres);
-
         boutonPartie = findViewById(R.id.bouton_partie);
-
+        boutonParametres = findViewById(R.id.bouton_parametres);
+        boutonConnexion = findViewById(R.id.bouton_connexion);
     }
 
     private void demanderActions() {
-
-        actionParametres = ControleurAction.demanderAction(GCommande.OUVRIR_MENU_PARAMETRES);
-
         actionPartie = ControleurAction.demanderAction(GCommande.DEMARRER_PARTIE);
-
+        actionParametres = ControleurAction.demanderAction(GCommande.OUVRIR_MENU_PARAMETRES);
+        actionConnexion = ControleurAction.demanderAction(GCommande.CONNEXION);
     }
 
 
     private void installerListeners() {
-
-        installerListenerParametres();
-
         installerListenerPartie();
-
+        installerListenerParametres();
+        installerListenerConnexion();
     }
 
     private void installerListenerPartie() {
-
         boutonPartie.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 actionPartie.executerDesQuePossible();
             }
         });
-
     }
 
     private void installerListenerParametres() {
-
         boutonParametres.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 actionParametres.executerDesQuePossible();
             }
         });
-
     }
 
+    private void installerListenerConnexion() {
+        boutonConnexion.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionConnexion.executerDesQuePossible();
+            }
+        });
+    }
 }
